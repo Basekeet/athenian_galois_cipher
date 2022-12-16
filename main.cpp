5c640c70e7f42c482cfc79def5c64a84c1bc7d45
rmalyushev_1 <rmalyushev_1@miem.hse.ru>
2022-12-16 16:34:05 +0300

    added main

--- main.cpp
+++ main.cpp
@@ -1,23 +1,92 @@
 #include <iostream>
+#include <string>
 #include <fstream>
 #include "caesar_cipher.h"
-// #include "athenian_cipher.h"
+#include "athenian_cipher.h"
  #include "recurrent_cipher.h"
 
 int main() {
-    RecurrentCipher c(11, 5, 13, 29);
+    int mode;
+    int ci;
 
-    // std::ifstream in("input.txt");
+    std::cout << "1 encode, 2 decode, 3 crack\n";
+    std::cin >> mode;
 
-    // std::string res;
-    // for (std::string line; std::getline(in, line);) {
-    //     res += line + " ";
-    // }
+    std::cout << "1,2 athenian,3 recurrent \n";
+    std::cin >> ci;
 
-    std::string s = c.encode("KilluaGon");
-    std::string d = c.decode(s);
-    std::cout << s << "\n";
-    std::cout << d << "\n";
-    // std::cout << s << "\n";
-    // std::cout << c.crack(s) << "\n";
+    if (mode == 1) {
+        if (ci == 1) {
+            std::cout << "key:\n";
+            std::string s;
+            std::cin >> s;
+            CaesarCipher c(s);
+            std::cout << "STR: ";
+            std::cin >> s;
+            std::cout << c.encode(s) << "\n";
+        } else if (ci == 2) {
+            std::cout << "keys:\n";
+            galois_num g1, g2;
+            std::cin >> g1 >> g2;
+            AthenianCipher c(g1, g2);
+            std::cout << "STR: ";
+            std::string s;
+            std::cin >> s;
+            std::cout << c.encode(s) << "\n";
+        } else if (ci == 3) {
+            std::cout << "keys:\n";
+            galois_num g1, g2, g3, g4;
+            std::cin >> g1 >> g2 >> g3 >> g4;
+            RecurrentCipher c(g1, g2, g3, g4);
+            std::cout << "STR: ";
+            std::string s;
+            std::cin >> s;
+            std::cout << c.encode(s) << "\n";
+        }
+    } else if (mode == 2) {
+        if (ci == 1) {
+            std::cout << "key:\n";
+            std::string s;
+            std::cin >> s;
+            CaesarCipher c(s);
+            std::cout << "STR: ";
+            std::cin >> s;
+            std::cout << c.decode(s) << "\n";
+        } else if (ci == 2) {
+            std::cout << "keys:\n";
+            galois_num g1, g2;
+            std::cin >> g1 >> g2;
+            AthenianCipher c(g1, g2);
+            std::cout << "STR: ";
+            std::string s;
+            std::cin >> s;
+            std::cout << c.decode(s) << "\n";
+        } else if (ci == 3) {
+            std::cout << "keys:\n";
+            galois_num g1, g2, g3, g4;
+            std::cin >> g1 >> g2 >> g3 >> g4;
+            RecurrentCipher c(g1, g2, g3, g4);
+            std::cout << "STR: ";
+            std::string s;
+            std::cin >> s;
+            std::cout << c.decode(s) << "\n";
+        }
+    } else if (mode == 3) {
+        if (ci == 2) {
+            AthenianCipher c(0, 0);
+            std::cout << "STR: ";
+            std::string s;
+            std::cin >> s;
+            std::cout << c.crack(s);
+        } else if (ci == 3) {
+            std::cout << "keys:\n";
+            galois_num g1, g2, g3, g4;
+            std::cin >> g1 >> g2 >> g3 >> g4;
+            RecurrentCipher c(g1, g2, g3, g4);
+            std::cout << "STR: ";
+            std::string s;
+            std::cin >> s;
+            std::cout << c.crack(s) << "\n";
+        }
+    }
 }
